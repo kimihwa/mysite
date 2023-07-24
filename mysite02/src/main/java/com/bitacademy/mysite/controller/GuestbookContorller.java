ef6e5118935ef805bfa096514c532a06bfbc399c
김이화 <@DESKTOP-OAHFS92>
2023-07-24 16:19:50 +0900

    mysite02

---
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/GuestbookContorller.java
@@ -0,0 +1,52 @@
+package com.bitacademy.mysite.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.mysite.dao.GuestbookDao;
+import com.bitacademy.mysite.vo.GuestbookVo;
+
+public class GuestbookContorller {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+		
+		if("delete".equals(actionName)) {
+			String sno = request.getParameter("no");
+			Long no = Long.parseLong(sno);
+			String password = request.getParameter("password");
+			
+			new GuestbookDao().deleteByNoAndPassword(no, password);
+			response.sendRedirect(request.getContextPath() + "/guestbook");	
+		} else if("deleteform".equals(actionName)) {
+			request.getRequestDispatcher("/WEB-INF/views/guestbook/deleteform.jsp").forward(request, response);
+		} else if("add".equals(actionName)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String message = request.getParameter("message");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setMessage(message);
+			
+			new GuestbookDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/guestbook");		
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+		
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("/WEB-INF/views/guestbook/list.jsp").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
